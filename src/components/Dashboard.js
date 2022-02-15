90ec1467a2d4e2972a96c84985472e7fe948c279
sahil <sahilarora.2567@gmail.com>
2022-02-15 10:07:36 +0530

    push

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -16,6 +16,11 @@ import "react-chat-widget/lib/styles.css";
 const Dashboard = (props) => {
   const [date, setDate] = useState();
   const [userType, setUserType] = useState("");
+  const d=new Date();
+  const e=new Date();
+  let Currmonth = d.getMonth();
+  let lastDate=new Date(e.setFullYear(d.getFullYear()-1));
+  const [month,setMonth]=useState(String(Currmonth));
   const coreContext = useContext(CoreContext);
   let zero = [];
   const nine = [];
@@ -24,7 +29,15 @@ const Dashboard = (props) => {
   const fiftynine = [];
   const sixty = [];
   const inactive = [];
-
+  let zero1 = [];
+  const nine1 = [];
+  const nineteen1 = [];
+  const thirtynine1 = [];
+  const fiftynine1 = [];
+  const sixty1 = [];
+  
+  const months = [ "January", "February", "March", "April", "May", "June",
+"July", "August", "September", "October", "November", "December" ];
   let patientwdevice = [];
 
   useEffect(coreContext.checkLocalAuth, []);
@@ -64,6 +77,19 @@ const Dashboard = (props) => {
     coreContext.fetchBloodPressure(patientId, userType);
   };
   useEffect(fetchWeight, [coreContext.weightData.length]);
+  const renderSelect=()=>{
+    return(
+      <select class="form-select form-select-sm" aria-label=".form-select-sm example" value={month} onChange={(e)=>{setMonth(e.target.value);}}>
+        <option selected >Select the Month</option>
+      {months.map((curr,index)=>{
+        return(<option value={index}>{curr}</option>)
+        
+ })}
+      </select>)
+
+    
+  }
+  const selectmonth=React.useMemo(()=>renderSelect(),[month])
 
   console.log("sahilwight", coreContext.weightData);
   const wd = coreContext.weightData
@@ -108,21 +134,27 @@ const Dashboard = (props) => {
       );
     }
     if (coreContext.patients.length > 0) {
-      console.log(
-        "check dashbpoard patient",
-        coreContext.patients,
-        coreContext.AlltimeLogData
-      );
+      
       coreContext.patients.map((curr) => {
         let patientTimelog = coreContext.AlltimeLogData.filter(
-          (app) => app.UserId == curr.userId
+          (app) => app.UserId == curr.userId && Number(app.performedOn.substring(5,7))==Number(month)+1
         );
-        console.log("patient time log", patientTimelog);
+        // let patientTimelog = patientTimelogAll.filter(
+        //   (app) => Number(app.performedOn.substring(5,7))==Number(month)+1       );
+        patientTimelog.map((curr)=>{
+          console.log("patient time log app",new Date(curr.performedOn).getMilliseconds()> lastDate.getMilliseconds());
+        })
+        
         if (patientTimelog.length > 0) {
           let totalTimeLog = 0;
+          let totalTimeLogForDataReview = 0;
 
           patientTimelog.map((timelog) => {
-            totalTimeLog = Number(timelog.timeAmount) + totalTimeLog;
+            if(timelog.taskType==="CareCoordination"){
+              totalTimeLog = Number(timelog.timeAmount) + totalTimeLog;
+            }
+            else if(timelog.taskType==="DataReview")
+            totalTimeLogForDataReview = Number(timelog.timeAmount) + totalTimeLogForDataReview;
           });
           console.log("checking timelog", totalTimeLog);
           if (totalTimeLog >= 0 && totalTimeLog <= 60) {
@@ -148,12 +180,39 @@ const Dashboard = (props) => {
             sixty.push(curr.userId);
             //nine=nine+1;
           }
+          if (totalTimeLogForDataReview >= 0 && totalTimeLogForDataReview <= 60) {
+            zero1.push(curr.userId);
+          } else if (totalTimeLogForDataReview >= 60 && totalTimeLogForDataReview <= 540) {
+            // setOnetonine(onetonine+1)
+            nine1.push(curr);
+            //nine=nine+1;
+          } else if (totalTimeLogForDataReview >= 600 && totalTimeLogForDataReview <= 1140) {
+            // setOnetonine(onetonine+1)
+            nineteen1.push(curr);
+            //nine=nine+1;
+          } else if (totalTimeLogForDataReview >= 1200 && totalTimeLogForDataReview <= 2340) {
+            // setOnetonine(onetonine+1)
+            thirtynine1.push(curr);
+            //nine=nine+1;
+          } else if (totalTimeLogForDataReview >= 2400 && totalTimeLogForDataReview <= 3540) {
+            // setOnetonine(onetonine+1)
+            fiftynine1.push(curr.userId);
+            //nine=nine+1;
+          } else if (totalTimeLogForDataReview >= 3600) {
+            // setOnetonine(onetonine+1)
+            console.log("sixty1",curr)
+            sixty1.push(curr.userId);
+            //nine=nine+1;
+          }
+         
         } else {
           inactive.push(curr.userId);
         }
+        
       });
     }
   };
+  
 
   const renderRemotePatientMonitor = () => {
     if (
@@ -227,6 +286,10 @@ const Dashboard = (props) => {
             </div> */}
         <div className="col-md-3">
           <input type="checkbox" /> Show Only My Patients
+        </div>
+        <div className="col-md-4">
+        
+{selectmonth}
         </div>
       </div>
       <div className="card_body">
@@ -302,6 +365,68 @@ const Dashboard = (props) => {
             <th style={{ textAlign: "center" }}>
               <a href="/dpatients" onClick={() => setPatient(inactive)}>
                 {inactive.length}
+              </a>
+            </th>
+            {/* <th style={{ textAlign: 'center' }}><a href="/dpatients">{zeromin}</a></th> */}
+          </tr>
+        </table>
+      </div>
+      <div className="card_body">
+        <h5>
+          <Bezier /> Data Review & Management
+        </h5>
+        <table className="table table-bordered table-sm">
+          <tr>
+            
+            <th style={{ textAlign: "center" }}>Patients Enrolled</th>
+            <th style={{ textAlign: "center" }}>60+ Mins</th>
+
+            <th style={{ textAlign: "center" }}>40-59 Mins</th>
+            <th style={{ textAlign: "center" }}>20-39 Mins</th>
+            <th style={{ textAlign: "center" }}>10-19 Mins</th>
+            <th style={{ textAlign: "center" }}>1-9 Mins</th>
+            <th style={{ textAlign: "center" }}>0 Mins</th>
+            <th style={{ textAlign: "center" }}>Inactive</th>
+            <th style={{ textAlign: "center" }}>Not Enrolled</th>
+          </tr>
+          {renderTimeLogs()}
+          <tr>
+            <th style={{ textAlign: "center" }}>
+              <a href="/patients">{coreContext.patients.length}</a>{" "}
+            </th>
+            <th style={{ textAlign: "center" }}>
+              <a href="/dpatients" onClick={() => setPatient([...new Set(sixty1)])}>
+                {[...new Set(sixty1)].length}
+              </a>
+            </th>
+            <th style={{ textAlign: "center" }}>
+              <a href="/dpatients" onClick={() => setPatient([...new Set(fiftynine1)])}>
+                {[...new Set(fiftynine1)].length}
+              </a>
+            </th>
+            <th style={{ textAlign: "center" }}>
+              <a href="/dpatients" onClick={() => setPatient([...new Set(thirtynine1)])}>
+                {[...new Set(thirtynine1)].length}
+              </a>
+            </th>
+            <th style={{ textAlign: "center" }}>
+              <a href="/dpatients" onClick={() => setPatient([...new Set(nineteen1)])}>
+                {[...new Set(nineteen1)].length}
+              </a>
+            </th>
+            <th style={{ textAlign: "center" }}>
+              <a href="/dpatients" onClick={() => setPatient([...new Set(nine1)])}>
+                {[...new Set(nine1)].length}
+              </a>
+            </th>
+            <th style={{ textAlign: "center" }}>
+              <a href="/dpatients" onClick={() => setPatient([...new Set(zero1)])}>
+                {[...new Set(zero1)].length}
+              </a>
+            </th>
+            <th style={{ textAlign: "center" }}>
+              <a href="/dpatients" onClick={() => setPatient(inactive)}>
+                {[...new Set(inactive)].length}
               </a>
             </th>
             {/* <th style={{ textAlign: 'center' }}><a href="/dpatients">{zeromin}</a></th> */}
